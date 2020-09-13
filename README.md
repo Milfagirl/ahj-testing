d12e769fffa2f237fae7c1985ea2ff7854ceb6cc
Svetlana Ryabova <ganyim@inbox.ru>
2020-09-13 14:57:05 +0500

    init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ahj-testing
+[![Build status](https://ci.appveyor.com/api/projects/status/p13h946ttvwvp7ns?svg=true)](https://ci.appveyor.com/project/Milfagirl/ahj-testing)
+[!demo page](https://github/Milfagirl/ahj-testing)
---
+++ src/js/validate.js
@@ -0,0 +1,48 @@
+/* eslint-disable spaced-comment */
+/* eslint-disable default-case */
+/* eslint-disable operator-assignment */
+/* eslint-disable no-else-return */
+/* eslint no-mixed-operators: "error"*/
+
+export default function validate(data) {
+  const value = data.trim();
+  const newvalue = value.slice(0, -1);
+  const control = Number(value[value.length - 1]);
+  let sum = 0;
+  let lunachek;
+  let payment = '';
+  const array = [...newvalue].map((name) => Number(name));
+  for (let i = array.length - 1; i >= 0; i -= 2) {
+    array[i] = array[i] * 2;
+    if (array[i] > 9) {
+      array[i] = Math.trunc(array[i] / 10) + (array[i] % 10);
+    }
+  }
+  array.forEach((elem) => {
+    sum += elem;
+  });
+  sum *= 9;
+  if ((sum % 10) === control) {
+    lunachek = true;
+    switch (Number(value[0])) {
+      case 2:
+        payment = 'mir';
+        break;
+      case 3:
+        payment = 'amex';
+        break;
+      case 4:
+        payment = 'visa';
+        break;
+      case 5:
+        payment = 'master';
+        break;
+      case 6:
+        payment = 'maestro';
+        break;
+    }
+  } else {
+    lunachek = false;
+  }
+  return [lunachek, payment];
+}
